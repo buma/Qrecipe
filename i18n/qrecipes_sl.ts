48438189833cc3d6d6b879e3eac797b93b4ed03b
Marko Burjek <s0mebody.slo@gmail.com>
2015-12-27 23:59:55 +0100

    Update translations

--- i18n/qrecipes_sl.ts
+++ i18n/qrecipes_sl.ts
@@ -4,50 +4,64 @@
 <context>
     <name>MainForm.ui</name>
     <message>
-        <location filename="../MainForm.ui.qml" line="17"/>
         <source>Press Me 1</source>
-        <translation>Pritisni me 1</translation>
+        <translation type="vanished">Pritisni me 1</translation>
     </message>
     <message>
-        <location filename="../MainForm.ui.qml" line="22"/>
         <source>Press Me 2</source>
-        <translation>Pritisni me 2</translation>
+        <translation type="vanished">Pritisni me 2</translation>
+    </message>
+</context>
+<context>
+    <name>RecipeFullItem</name>
+    <message>
+        <location filename="../RecipeFullItem.qml" line="34"/>
+        <source>Ingredients</source>
+        <translation>Sestavine</translation>
+    </message>
+    <message>
+        <location filename="../RecipeFullItem.qml" line="53"/>
+        <source>Yields:</source>
+        <translation>Za:</translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
-        <location filename="../main.qml" line="9"/>
+        <location filename="../main.qml" line="10"/>
         <source>Hello World</source>
         <translation>Pozdravljen Svet</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="13"/>
+        <location filename="../main.qml" line="14"/>
         <source>File</source>
         <translation>Datoteka</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="15"/>
+        <location filename="../main.qml" line="16"/>
         <source>&amp;Open</source>
         <translation>&amp;Odpri</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="19"/>
+        <location filename="../main.qml" line="20"/>
         <source>Exit</source>
         <translation>Izhod</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="27"/>
+        <location filename="../main.qml" line="24"/>
+        <source>Back</source>
+        <translation>Nazaj</translation>
+    </message>
+    <message>
         <source>Button 1 pressed</source>
-        <translation>Gumb 1 je bil pritisnjen</translation>
+        <translation type="vanished">Gumb 1 je bil pritisnjen</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="28"/>
         <source>Button 2 pressed</source>
-        <translation>Gumb 2 je bil pritisnjen</translation>
+        <translation type="vanished">Gumb 2 je bil pritisnjen</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="33"/>
+        <location filename="../main.qml" line="67"/>
         <source>May I have your attention, please?</source>
         <translation>Ali imam lahko vašo pozornost?</translation>
     </message>
