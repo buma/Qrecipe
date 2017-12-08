8f653f21729164109149b6be9c1686a8983770ed
Marko Burjek <s0mebody.slo@gmail.com>
2017-12-08 18:06:24 +0100

    Updated translations

--- i18n/qrecipes_sl.ts
+++ i18n/qrecipes_sl.ts
@@ -15,42 +15,46 @@
 <context>
     <name>RecipeFullItem</name>
     <message>
-        <location filename="../RecipeFullItem.qml" line="34"/>
+        <location filename="../RecipeFullItem.qml" line="35"/>
         <source>Ingredients</source>
         <translation>Sestavine</translation>
     </message>
     <message>
-        <location filename="../RecipeFullItem.qml" line="53"/>
+        <location filename="../RecipeFullItem.qml" line="50"/>
         <source>Yields:</source>
         <translation>Za:</translation>
     </message>
 </context>
 <context>
+    <name>RecipeSmallItem</name>
+    <message>
+        <location filename="../RecipeSmallItem.qml" line="71"/>
+        <source>Close</source>
+        <translation>Zapri</translation>
+    </message>
+</context>
+<context>
     <name>main</name>
     <message>
-        <location filename="../main.qml" line="10"/>
+        <location filename="../main.qml" line="11"/>
         <source>Hello World</source>
         <translation>Pozdravljen Svet</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="14"/>
         <source>File</source>
-        <translation>Datoteka</translation>
+        <translation type="vanished">Datoteka</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="16"/>
         <source>&amp;Open</source>
-        <translation>&amp;Odpri</translation>
+        <translation type="vanished">&amp;Odpri</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="20"/>
         <source>Exit</source>
-        <translation>Izhod</translation>
+        <translation type="vanished">Izhod</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="24"/>
         <source>Back</source>
-        <translation>Nazaj</translation>
+        <translation type="vanished">Nazaj</translation>
     </message>
     <message>
         <source>Button 1 pressed</source>
@@ -61,7 +65,7 @@
         <translation type="vanished">Gumb 2 je bil pritisnjen</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="67"/>
+        <location filename="../main.qml" line="62"/>
         <source>May I have your attention, please?</source>
         <translation>Ali imam lahko vašo pozornost?</translation>
     </message>
